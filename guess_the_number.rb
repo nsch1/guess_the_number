164a50a672aae7c1766d74429d29b35d7d149a54
nsch1 <x>
2018-02-14 16:51:07 +0100

    Adds loop with conditionals to check if the guess is correct

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -2,5 +2,20 @@ secret_number = 1 + rand(100)
 
 def guess
 	print "Make a guess: "
-	gets.to_i
+	@guess = gets.to_i
+end
+
+guess
+
+loop do
+	if @guess == secret_number
+		puts "You win!"
+		break
+	elsif @guess < secret_number
+		puts "Higher!"
+		guess
+	else
+		puts "Lower!"
+		guess
+	end
 end
