652e5626d898629d344d56ef0f28e3caa6f26f40
nsch1 <x>
2018-02-14 17:08:54 +0100

    Refactoring of the loop

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,21 +1,23 @@
 secret_number = 1 + rand(100)
+attempts = 10
 
 def guess
 	print "Make a guess: "
 	@guess = gets.to_i
 end
 
-guess
-
-loop do
+until attempts == 0 do
+	guess
 	if @guess == secret_number
-		puts "You win!"
+		puts "You win! You have #{attempts*10} points left!"
 		break
 	elsif @guess < secret_number
 		puts "Higher!"
-		guess
+		attempts -= 1
 	else
 		puts "Lower!"
-		guess
+		attempts -= 1
 	end
 end
+
+puts "GAME OVER!"
